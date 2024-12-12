1bfd55e358d7f46be31eff9994d7887ce7b7e9bb
Aban <khanaban505@gmail.com>
2024-12-12 16:41:23 -0500

    changed box back

--- main_simple.py
+++ main_simple.py
@@ -116,8 +116,8 @@ def init_scene():
     back_wall = BoxObj()
     back_wall.name = "Back Wall"
     back_wall.set_material(mat)
-    back_wall.scale(20, 14, 0.1)
-    back_wall.translate(0, 1.5, -200)
+    back_wall.scale(10, 7, 0.1)
+    back_wall.translate(0, 0.5, -100)
     scn.add_object(back_wall)
 
     #front wall
@@ -126,8 +126,8 @@ def init_scene():
     frontwall = BoxObj()
     frontwall.name = "frontwall"
     frontwall.set_material(mat)
-    frontwall.scale(20, 14, 0.1)
-    frontwall.translate(0, 1, 200)
+    frontwall.scale(10, 7, 0.1)
+    frontwall.translate(0, 0.5, 100)
     scn.add_object(frontwall)
 
     # Left Wall
@@ -136,8 +136,8 @@ def init_scene():
     left_wall = BoxObj()
     left_wall.name = "Left Wall"
     left_wall.set_material(mat)
-    left_wall.scale(0.1, 14, 20)
-    left_wall.translate(200, 1.0, 0)
+    left_wall.scale(0.1, 7, 10)
+    left_wall.translate(100, 0.5, 0)
     #left_wall.rotate(90,Vector3(0,0,0))
     scn.add_object(left_wall)
 
@@ -147,8 +147,8 @@ def init_scene():
     right_wall = BoxObj()
     right_wall.name = "Right Wall"
     right_wall.set_material(mat)
-    right_wall.scale(0.1, 14, 20)
-    right_wall.translate(-200, 1.0, 0)
+    right_wall.scale(0.1, 7, 10)
+    right_wall.translate(-100, 0.5, 0)
     scn.add_object(right_wall)
 
     # Ceiling
@@ -157,8 +157,8 @@ def init_scene():
     ceiling = BoxObj()
     ceiling.name = "Ceiling"
     ceiling.set_material(mat)
-    ceiling.scale(20, 0.1, 20)
-    ceiling.translate(0, 200, 0)
+    ceiling.scale(10, 0.1, 10)
+    ceiling.translate(0, 100, 0)
     scn.add_object(ceiling)
 
     # Create and add a sphere
